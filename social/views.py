7f88e0caf96829ee7c5713c644585f1ee06c74ee
Andi M <zapp@labs.com>
2020-05-10 23:33:38 -0400

    a list of users

--- social/views.py
+++ social/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.views.generic import TemplateView
 # Create your views here.
 
-def index(request):
-    return render(request, 'social/index.html')
+class IndexView (TemplateView):
+    template_name =  'social/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['house_users'] = User.objects.all()[:5]
+        return context
 
 def profile(request):
     return render(request, 'social/profile.html')
